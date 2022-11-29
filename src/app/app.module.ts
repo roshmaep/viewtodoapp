233ec634d2eaf33b055e4b93542084d21ea0b9b3
roshmaep <roshmaep1999@gmail.com>
2022-11-29 14:59:33 +0530

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { ViewtodoComponent } from './viewtodo/viewtodo.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule} from '@angular/common/http';
 const myRoute:Routes=[
   {
     path:"",
@@ -31,7 +31,8 @@ const myRoute:Routes=[
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(myRoute),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
